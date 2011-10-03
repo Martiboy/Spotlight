8939bc272e20180e7a8f57c59cc3992d80f4f293
ruchmaster <ruch_master_92@hotmail.com>
2011-10-03 14:49:32 +0200

    everything

---
+++ Rocha/opdracht auke by rocha.py
@@ -0,0 +1,5 @@
+print ("Hoi ik ben <je naam> en als ik iets moet uitrekenen in mijn programma is dat geen probleem kijk maar:") 
+print("5 * 5 = {0}".format(5*5))
+print("1234 + 23 = {0}".format(1234 + 23))
+print("15 / 47 = {0}".format(15 / 47))
+print("23% 4325 = {0}".format(23% 4325))
---
+++ test2.py
@@ -0,0 +1,14 @@
+#variables demonstrated
+print ("This program is a demo of variables")
+v = 1
+print ("The value of v is now", v)
+v = v + 1
+print ("v now equals itself plus one, making it worth", v)
+v = 51
+print ("v can store any numerical value, to be used elsewhere.")
+print ("for example, in a sentence. v is now worth", v)
+print ("v times 5 equals", v*5)
+print ("but v still only remains", v)
+print ("to make v five times bigger, you would have to type v = v * 5")
+v = v * 5
+print ("there you go, now v equals", v, "and not", v / 5)
